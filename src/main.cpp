13e36aadfbd9beec124cbc5b2efd712c013c4f7c
Diogo <email>
2024-04-03 11:56:55 +0100

    comments on socket last parameter

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,7 @@ int main(int ac, char *av[]) {
 
 		// We use AF_INET because it's the only compatible with IPv4
 		// We use stream socket because it's the only compatible with TCP
+		// We use IPPROTO_TCP because it's the only compatible with TCP (same as getprotobyname())
 	int sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	fcntl(sockfd, F_SETFL, O_NONBLOCK); // Makes the socket non-blocking, as the subject requires
 	std::cout << "socket fd created: " << sockfd << std::endl;
