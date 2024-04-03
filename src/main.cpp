bd9f66ef816fc49337cf5dac5e6b03d31bfafe3b
lopjf <loris.penelon@outlook.com>
2024-04-03 12:02:14 +0100

    ok

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,14 @@
 #include "../lib/Client.hpp"
 #include "../lib/Server.hpp"
 
-
-
 int main(int ac, char *av[]) {
+    Client clien;
     Server serv;
 	
     if (ac != 3) {
         std::cout << "The program takes 2 arguments. Call the program like so: ./ircserv <port> <password>" << std::endl;
         return 0;
     }
-    // Server::_signal = false;
 
 		// We use AF_INET because it's the only compatible with IPv4
 		// We use stream socket because it's the only compatible with TCP
@@ -20,6 +18,7 @@ int main(int ac, char *av[]) {
 	fcntl(sockfd, F_SETFL, O_NONBLOCK); // Makes the socket non-blocking, as the subject requires
 	std::cout << "socket fd created: " << sockfd << std::endl;
 	
+    (void)clien;
     (void)serv;
     (void)av;
     return 0;
