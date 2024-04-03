dfc3f11d39ddd0792be7ec8729384cbe448e90a4
Diogo <email>
2024-04-03 11:55:58 +0100

    socket now created good

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,8 @@ int main(int ac, char *av[]) {
 
 		// We use AF_INET because it's the only compatible with IPv4
 		// We use stream socket because it's the only compatible with TCP
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	int sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	fcntl(sockfd, F_SETFL, O_NONBLOCK); // Makes the socket non-blocking, as the subject requires
-	
 	std::cout << "socket fd created: " << sockfd << std::endl;
 	
     (void)clien;
