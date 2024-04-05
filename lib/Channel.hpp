cf0bc6cfc02f9796e8850920aaf0afdc97e276b9
lopjf <loris.penelon@outlook.com>
2024-04-05 15:44:39 +0100

    channel clients and operators

--- lib/Channel.hpp
+++ lib/Channel.hpp
@@ -1,5 +1,39 @@
 #ifndef CHANNEL_HPP
 #define CHANNEL_HPP
 
+#include <string>
+#include <map>
+#include <vector>
+
+#include "Client.hpp"
+
+
+class Client;
+
+class Channel {
+    private:
+        std::string _name;
+        std::string _topic;
+        std::map<int, Client *> _clients;
+        std::map<int, Client *> _operators;
+
+    public:
+        Channel();
+
+        void setName(std::string name) {_name = name;};
+        std::string getName() {return _name;};
+
+        void setTopic(std::string topic) {_topic = topic;};
+        std::string getTopic() {return _topic;};
+
+        void addClient(Client &client);
+		void removeClient(Client &client);
+        bool isClient(Client &client);
+
+        void addOperator(Client &client);
+		void removeOperator(Client &client);
+        bool isOperator(Client &client);
+
+};
 
 #endif
--- src/Channel.cpp
+++ src/Channel.cpp
@@ -1,1 +1,21 @@
 #include "../lib/Channel.hpp"
+
+void Channel::addClient(Client &client) {
+    _clients[client.getFd()] = &client;
+}
+void Channel::removeClient(Client &client) {
+    _clients.erase(_clients.find(client.getFd()));
+}
+bool Channel::isClient(Client &client) {
+    return (_clients.find(client.getFd()) != _clients.end());
+}
+
+void Channel::addOperator(Client &client) {
+    _operators[client.getFd()] = &client;
+}
+void Channel::removeOperator(Client &client) {
+    _operators.erase(_operators.find(client.getFd()));
+}
+bool Channel::isOperator(Client &client) {
+    return (_operators.find(client.getFd()) != _clients.end());
+}
